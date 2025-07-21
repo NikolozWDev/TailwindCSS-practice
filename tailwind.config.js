87a5f04e067de1876470fa82f3b0a5b2ee3bb599
Nikoloz Gigiashvili <gigiashvilinikoloz@gmail.com>
2025-07-21 09:00:26 +0400

    added more styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      xxl: '1536px',
+    },
     extend: {
       colors: {
         primary: '#cc7596'
